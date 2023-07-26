011203753a220944c6862924aed71429223ce296
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-07-26 23:59:00 +0000

    Move `_evaluate` tests into a class

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -3,36 +3,31 @@ import pytest
 from lisp_evaluator import _evaluate, EvaluationError
 
 
-def test_number():
-    assert _evaluate(42) == 42
+class Test_evaluate:
+    def test_number(self):
+        assert _evaluate(42) == 42
 
+    def test_not_list(self):
+        with pytest.raises(EvaluationError) as error:
+            _evaluate("Not a list")
+        assert (
+            str(error.value)
+            == "This interpreter only accepts integers and operations, for now."
+        )
 
-def test_not_list():
-    with pytest.raises(EvaluationError) as error:
-        _evaluate("Not a list")
-    assert (
-        str(error.value)
-        == "This interpreter only accepts integers and operations, for now."
-    )
+    def test_empty_ast(self):
+        assert _evaluate([]) is None
 
+    def test_add(self):
+        assert _evaluate(["+", 23, 19]) == 42
 
-def test_empty_ast():
-    assert _evaluate([]) is None
+    def test_substract(self):
+        assert _evaluate(["-", 51, 9]) == 42
 
+    def test_multiply(self):
+        assert _evaluate(["*", 6, 7]) == 42
 
-def test_add():
-    assert _evaluate(["+", 23, 19]) == 42
-
-
-def test_substract():
-    assert _evaluate(["-", 51, 9]) == 42
-
-
-def test_multiply():
-    assert _evaluate(["*", 6, 7]) == 42
-
-
-def test_unknown_operation():
-    with pytest.raises(EvaluationError) as error:
-        _evaluate(["%", 142, 100])
-    assert str(error.value).startswith("This operation doesn't exist: ")
+    def test_unknown_operation(self):
+        with pytest.raises(EvaluationError) as error:
+            _evaluate(["%", 142, 100])
+        assert str(error.value).startswith("This operation doesn't exist: ")
